2de180be4df8dcf13e81bbc2a25f23fa2bd17938
Rafael Tavares Carvalho Barros <rafael_tcb@hotmail.com>
2017-11-10 23:28:40 -0200

    Ajustes no CSS
    Melhoria no CSS dos inputs

--- cadastro.php
+++ cadastro.php
@@ -2,9 +2,13 @@
 <html lang="pt-br">
     <head>
         <meta charset="utf-8">
-        <link id="css" rel="stylesheet" type="text/css" href="style1/style.css">
+
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"> -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+
+        <link id="css" rel="stylesheet" type="text/css" href="style1/style.css">
         <title>Cadastro</title>
     </head>
     <body>
@@ -12,11 +16,11 @@
             <li class="menuItem"><a href="cadastro.php" class="ativo">Cadastro</a></li>
             <li class="menuItem"><a href="consulta.php">Consulta</a></li>
             <li class="dropdown">
-                <a href="javascript:void(0)" class="dropbtn">Estilos</a>
+                <a href="" class="dropbtn">Estilos</a>
                 <div class="dropdown-conteudo">
-                    <a href="#" class="dropdown-conteudo-a waves-effect waves-light">Principal</a>
+                    <a href="#" class="dropdown-conteudo-a ">Principal</a>
                 </div>
-                <li style="float:right" class="menuItem"><a href="#about">Sair</a></li>
+                <li style="float:right" class="menuItem"><a href="#about"><span class="glyphicon glyphicon-log-out"></span>&nbsp&nbsp Sair</a></li>
             </li>
         </ul>
         <div id="cadastro"> 
@@ -28,10 +32,10 @@
                     </tr>
                     <tr>
                         <td>
-                            <input type="radio" name="status" value="cliente" checked="checked" /> <label>Cliente</label>
+                            <input type="radio" name="status" value="cliente" checked="checked" id="radioCliente"/> <label for="radioCliente" class="pointer">Cliente</label>
                         </td>
                         <td>
-                            <input type="radio" name="status" value="gerente" /> <label>Gerente</label>
+                            <input type="radio" name="status" value="gerente" id="radioGerente"/> <label for="radioGerente" class="pointer">Gerente</label>
                         </td>
                     </tr>
                     <tr>
@@ -41,7 +45,9 @@
                     </tr>
                     <tr>
                         <td colspan="2">
-                            <input type="text" name="nome" placeholder="Digite seu nome" class="linha-completa"/>
+                            <div class="input-group input-group-lg linha-completa">
+                                <input type="text" name="nome" placeholder="Digite seu nome" class="form-control linha-completa"/>
+                            </div>
                         </td>
                     </tr>
                     <tr>
@@ -54,10 +60,14 @@
                     </tr>
                     <tr>
                         <td>
-                            <input type="text" name="cpf" placeholder="Digite seu CPF" class="linha-dividida"/>
+                            <div class="input-group input-group-lg linha-dividida">
+                                <input type="text" name="cpf" placeholder="Digite seu CPF" class="form-control linha-dividida"/>
+                            </div>
                         </td>
                         <td>
-                            <input type="date" name="data" placeholder="Digite sua data de aniversário" class="linha-dividida"/>
+                            <div class="input-group input-group-lg linha-dividida">
+                                <input type="date" name="data" placeholder="Digite sua data de aniversário" class="form-control linha-dividida"/>
+                            </div>
                         </td>
                     <tr>
                         <td>
@@ -69,10 +79,14 @@
                     </tr>
                     <tr>
                         <td>
-                            <input type="text" name="telefone" placeholder="Digite seu número de telefone" class="linha-dividida"/>
+                            <div class="input-group input-group-lg linha-dividida">
+                                <input type="text" name="telefone" placeholder="Digite seu número de telefone" class="form-control linha-dividida"/>
+                            </div>
                         </td>
                         <td>
-                            <input type="email" name="email" placeholder="Digite seu e-mail" class="linha-dividida"/>
+                            <div class="input-group input-group-lg linha-dividida">
+                                <input type="email" name="email" placeholder="Digite seu e-mail" class="form-control linha-dividida"/>
+                            </div>
                         </td>
                     </tr>
 
@@ -92,10 +106,14 @@
                     </tr>
                     <tr>
                         <td>
-                            <input type="text" name="cep" placeholder="Digite seu CEP" class="linha-dividida"/>
+                            <div class="input-group input-group-lg linha-dividida">
+                                <input type="text" name="cep" placeholder="Digite seu CEP" class="form-control linha-dividida"/>
+                            </div>
                         </td>
                         <td>
-                            <input type="text" name="rua" placeholder="Digite sua rua" class="linha-dividida"/>
+                            <div class="input-group input-group-lg linha-dividida">
+                                <input type="text" name="rua" placeholder="Digite sua rua" class="form-control linha-dividida"/>
+                            </div>
                         </td>
                     </tr>
                     <tr>
@@ -108,10 +126,14 @@
                     </tr>
                     <tr>
                         <td>
-                            <input type="text" name="numero" placeholder="Digite o numero da residência" class="linha-dividida"/>
+                            <div class="input-group input-group-lg linha-dividida">
+                                <input type="text" name="numero" placeholder="Digite o numero da residência" class="form-control linha-dividida"/>
+                            </div>
                         </td>
                         <td>
-                            <input type="text" name="complemento" placeholder="Digite o complemento" class="linha-dividida"/>
+                            <div class="input-group input-group-lg linha-dividida">
+                                <input type="text" name="complemento" placeholder="Digite o complemento" class="form-control linha-dividida"/>
+                            </div>
                         </td>
                     </tr>
                     <tr>
@@ -124,38 +146,42 @@
                     </tr>
                     <tr>
                         <td>
-                            <input type="text" name="cidade" placeholder="Digite a cidade" class="linha-dividida"/>
+                            <div class="input-group input-group-lg linha-dividida">
+                                <input type="text" name="cidade" placeholder="Digite a cidade" class="form-control linha-dividida"/>
+                            </div>
                         </td>
                         <td>
-                            <select name="estado" class="linha-dividida">
-                                    <option>Acre</option>
-                                    <option>Alagoas</option>
-                                    <option>Amapá</option>
-                                    <option>Amazonas</option>
-                                    <option>Bahia</option>
-                                    <option>Ceará</option>
-                                    <option>Distirto Federal</option>
-                                    <option>Espírito Santo</option>
-                                    <option>Goiás</option>
-                                    <option>Maranhão</option>
-                                    <option>Mato Grosso</option>
-                                    <option>Mato Grosso do Sul</option>
-                                    <option>Minas Gerais</option>
-                                    <option>Pará</option>
-                                    <option>Paraíba</option>
-                                    <option>Paraná</option>
-                                    <option>Pernambuco</option>
-                                    <option>Piauí</option>
-                                    <option>Rio de Janeiro</option>
-                                    <option>Rio Grande do Norte</option>
-                                    <option>Rio Grande do Sul</option>
-                                    <option>Rondônia</option>
-                                    <option>Roraima</option>
-                                    <option>Santa Catarina</option>
-                                    <option>São Paulo</option>
-                                    <option>Sergipe</option>
-                                    <option>Tocantins</option>
-                            </select>
+                            <div class="input-group input-group-lg linha-dividida">
+                                <select name="estado" class="form-control linha-dividida">
+                                        <option>Acre</option>
+                                        <option>Alagoas</option>
+                                        <option>Amapá</option>
+                                        <option>Amazonas</option>
+                                        <option>Bahia</option>
+                                        <option>Ceará</option>
+                                        <option>Distirto Federal</option>
+                                        <option>Espírito Santo</option>
+                                        <option>Goiás</option>
+                                        <option>Maranhão</option>
+                                        <option>Mato Grosso</option>
+                                        <option>Mato Grosso do Sul</option>
+                                        <option>Minas Gerais</option>
+                                        <option>Pará</option>
+                                        <option>Paraíba</option>
+                                        <option>Paraná</option>
+                                        <option>Pernambuco</option>
+                                        <option>Piauí</option>
+                                        <option>Rio de Janeiro</option>
+                                        <option>Rio Grande do Norte</option>
+                                        <option>Rio Grande do Sul</option>
+                                        <option>Rondônia</option>
+                                        <option>Roraima</option>
+                                        <option>Santa Catarina</option>
+                                        <option>São Paulo</option>
+                                        <option>Sergipe</option>
+                                        <option>Tocantins</option>
+                                </select>
+                            </div>
                         </td>
                     </tr>
 
@@ -174,7 +200,9 @@
                     </tr>
                     <tr>
                         <td colspan="2">
-                            <input type="text" name="complemento" placeholder="Digite seu usuário" class="linha-completa"/>
+                            <div class="input-group input-group-lg linha-completa">
+                                <input type="text" name="complemento" placeholder="Digite seu usuário" class="form-control linha-completa"/>
+                            </div>
                         </td>
                     </tr>
                     <tr>
@@ -184,7 +212,9 @@
                     </tr>
                     <tr>
                         <td colspan="2">
-                            <input type="password" name="cidade" placeholder="Digite sua senha" class="linha-completa"/>
+                            <div class="input-group input-group-lg linha-completa">
+                                <input type="password" name="cidade" placeholder="Digite sua senha" class="form-control linha-completa"/>
+                            </div>
                         </td>
                     </tr>
                     <tr>
@@ -194,7 +224,9 @@
                     </tr>
                     <tr>
                         <td colspan="2">
-                            <input type="password" name="cidade" placeholder="Confirme sua senha" class="linha-completa"/>
+                            <div class="input-group input-group-lg linha-completa">
+                                <input type="password" name="cidade" placeholder="Confirme sua senha" class="form-control linha-completa"/>
+                            </div>
                         </td>
                     </tr>
                     <tr>
