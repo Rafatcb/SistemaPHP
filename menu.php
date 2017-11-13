c097f76c6a14a963d4df452f99a8be62eab7d4ea
Rafael Tavares Carvalho Barros <rafael_tcb@hotmail.com>
2017-11-13 12:15:57 -0200

    Início da tela de Consulta
    Início da tela de Consulta, com os filtros

---
+++ menu.php
@@ -0,0 +1,11 @@
+<ul class="menu">
+    <li class="menuItem"><a href="cadastro.php" class="ativo">Cadastro</a></li>
+    <li class="menuItem"><a href="consulta.php">Consulta</a></li>
+    <li class="dropdown">
+        <a href="" class="dropbtn">Estilos</a>
+        <div class="dropdown-conteudo">
+            <a href="#" class="dropdown-conteudo-a ">Principal</a>
+        </div>
+        <li style="float:right" class="menuItem"><a href="#about"><span class="glyphicon glyphicon-log-out"></span>&nbsp&nbsp Sair</a></li>
+    </li>
+</ul>
